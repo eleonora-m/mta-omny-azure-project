4e6406ee4e8f4f6373bdaf4fd667137db15ebb71
Eleonora <devops.nora@gmail.com>
2026-03-14 19:17:06 -0400

    "rg-mta-omny-v7" # Новое имя, чтобы избежать конфликтов

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,17 @@
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
-  default     = "rg-mta-omny-prod-v5"
+  default     = "rg-mta-omny-v7" # Новое имя, чтобы избежать конфликтов
 }
 
 variable "location" {
   description = "Azure region to deploy resources"
   type        = string
-  default     = "eastus2"
+  default     = "westus" # Смена региона на более свободный
 }
 
 variable "project_name" {
   description = "Prefix for all resources"
   type        = string
-  default     = "omny-fare-system"
+  default     = "mta-prod"
 }
